1204ed2999768c107d59f94c94cb4b3650010650
lukashevar <184438185+lukashevar@users.noreply.github.com>
2026-03-21 21:10:08 +0300

    Add files for submission of information

---
+++ lab1/Submission.cpp
@@ -0,0 +1,17 @@
+#include "Submission.h"
+
+Submission::Submission(const QuadraticEquation& eq, const Solution& sol, const std::string& name)
+	: equation(eq), solution(sol), studentName(name) {
+}
+
+const QuadraticEquation& Submission::getEquation() const {
+	return equation;
+}
+
+const Solution& Submission::getSolution() const {
+	return solution;
+}
+
+const std::string Submission::getStudentName() const {
+	return studentName;
+}
---
+++ lab1/Submission.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include "QuadraticEquation.h"
+#include "Solution.h"
+
+class Submission {
+private:
+	QuadraticEquation equation;
+	Solution solution;
+	std::string studentName;
+
+public:
+	Submission(const QuadraticEquation& eq, const Solution& sol, const std::string& name);
+
+	const QuadraticEquation& getEquation() const;
+	const Solution& getSolution() const;
+	const std::string getStudentName() const;
+};
