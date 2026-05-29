1a81f75ba50f23065264b9dd5ea3c106d96d7c83
lukashevar <184438185+lukashevar@users.noreply.github.com>
2026-05-29 22:51:46 +0300

    ++

--- lab4/Arcanoid/main.cpp
+++ lab4/Arcanoid/main.cpp
@@ -1,31 +1,9 @@
-
-#include <SFML/Graphics.hpp>
-
-using namespace sf;
+#include "include/core/Game.h"
 
 int main()
 {
+    Game game;
+    game.run();
 
-    RenderWindow window(VideoMode(400, 400), L"Новый проект", Style::Default);
-
-    window.setVerticalSyncEnabled(true);
-
-    CircleShape shape(100.f, 3);
-    shape.setPosition(100, 100);
-    shape.setFillColor(Color::Magenta);
-
-    while (window.isOpen())
-    {
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                window.close();
-        }
-
-        window.clear(Color::Blue);
-        window.draw(shape);
-        window.display();
-    }
     return 0;
 }
